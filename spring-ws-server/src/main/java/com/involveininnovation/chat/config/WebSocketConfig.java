ccd70ec8783de75b9079c925b8b256dbe414bdaf
unknown <aniketb6644@gmail.com>
2024-04-13 12:35:33 +0530

    dhd

--- spring-ws-server/src/main/java/com/involveininnovation/chat/config/WebSocketConfig.java
+++ spring-ws-server/src/main/java/com/involveininnovation/chat/config/WebSocketConfig.java
@@ -11,9 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS().setClientLibraryUrl("https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js");
     }
-
+    
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
